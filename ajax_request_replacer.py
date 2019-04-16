d7ebdc97b4fbba3698561cfb6aef4baa84c65522
George Benjamin-Schonberger <george.benjamin@gmail.com>
2019-04-16 23:54:13 +0300

    Optimization

--- ajax_request_replacer.py
+++ ajax_request_replacer.py
@@ -1,6 +1,13 @@
+import re
 from base_processor_adapter import BaseProcessorAdapter
 
 class ProcessorAdapter(BaseProcessorAdapter):
+    _target_regex = None
+
+    def __init__(self, rules=None, app_logger=None):
+        super(ProcessorAdapter, self).__init__(rules=rules, app_logger=app_logger)
+        _target_regex = re.compile(r'^\{\$\w+\}$')
+
     def prepareArgument(self, arg):
         arg = arg.strip('" \t\n')
         if ':' in arg:
@@ -67,15 +74,23 @@ class ProcessorAdapter(BaseProcessorAdapter):
         params = '{ ' + (params if isinstance(params, str) else '') + ' }'
         return { 'params': params, 'target_id': target_id, 'jparams': jparams, 'eparams': eparams }
 
+    def process_target_id_val(self, target_id):
+        if not isinstance(target_id, str) or len(target_id) == 0:
+            return "''"
+        matched = self._target_regex.search(target_id)
+        if isinstance(matched, re.Match) and matched.group() == target_id:
+            return target_id.strip('}{')
+        return "'{}'".format(target_id)
+
     def command_string(self, data, extra_params):
         prefix_length = extra_params['prefix_length'] if isinstance(extra_params['prefix_length'], int) else 0
         params = self.ajaxParamsStringConvert(data[prefix_length:].strip('," \t'))
-        return "'ajax_command'=>\"{}\",\n'ajax_target_id'=>'{}',".format(params['params'], params['target_id'])
+        return "'ajax_command'=>\"{}\",\n'ajax_target_id'=>{},".format(params['params'], self.process_target_id_val(params['target_id']))
 
     def onclick_str(self, data, extra_params):
         prefix_length = extra_params['prefix_length'] if isinstance(extra_params['prefix_length'], int) else 0
         params = self.ajaxParamsStringConvert(data[prefix_length:].strip('," \t'))
-        return "'onclick_ajax_command'=>\"{}\",\n'onclick_target_id'=>'{}',".format(params['params'], params['target_id'])
+        return "'onclick_ajax_command'=>\"{}\",\n'onclick_target_id'=>{},".format(params['params'], self.process_target_id_val(params['target_id']))
 
     def tab_ajax_content(self, data, extra_params):
         prefix_length = extra_params['prefix_length'] if isinstance(extra_params['prefix_length'], int) else 0
@@ -87,7 +102,7 @@ class ProcessorAdapter(BaseProcessorAdapter):
         data = data[prefix_length:]
         data = data.replace('"AjaxRequest(','').strip(');" \t')
         params = self.ajaxParamsStringConvert(data)
-        return "NApp::Ajax()->Execute(\"{}\",'{}');".format(params['params'], params['target_id'])
+        return "NApp::Ajax()->Execute(\"{}\",{});".format(params['params'], self.process_target_id_val(params['target_id']))
     
     def action_legacy_prepare(self, data, extra_params):
         prefix_length = extra_params['prefix_length'] if isinstance(extra_params['prefix_length'], int) else 0
@@ -96,12 +111,20 @@ class ProcessorAdapter(BaseProcessorAdapter):
         data = data.replace('"AjaxRequest(','').strip('){}" \t'.format(end_char))
         params = self.ajaxParamsStringConvert(data)
         action = extra_params['action'] if isinstance(extra_params['action'], str) else ''
-        return "'{}'=>NApp::Ajax()->Prepare(\"{}\",'{}'){}".format(action, params['params'], params['target_id'], end_char)
+        return "'{}'=>NApp::Ajax()->Prepare(\"{}\",{}){}".format(action, params['params'], self.process_target_id_val(params['target_id']), end_char)
 
     def echo_legacy_prepare(self, data, extra_params):
         prefix_length = extra_params['prefix_length'] if isinstance(extra_params['prefix_length'], int) else 0
         data = data[prefix_length:]
         data = data.replace('"AjaxRequest(','').strip(');" ?>\t')
         params = self.ajaxParamsStringConvert(data)
-        return "=\"<?=NApp::Ajax()->Prepare(\"{}\",'{}')?>\"".format(params['params'], params['target_id'])
+        return "=\"<?=NApp::Ajax()->Prepare(\"{}\",{})?>\"".format(params['params'], self.process_target_id_val(params['target_id']))
+
+    def variable_legacy_prepare(self, data, extra_params):
+        prefix_length = extra_params['prefix_length'] if isinstance(extra_params['prefix_length'], int) else 0
+        end_char = extra_params['end_char'] if isinstance(extra_params['end_char'], str) else ''
+        data = data[prefix_length:]
+        data = data.replace('"AjaxRequest(','').strip('){}" \t'.format(end_char))
+        params = self.ajaxParamsStringConvert(data)
+        return "=NApp::Ajax()->Prepare(\"{}\",{}){}".format(params['params'], self.process_target_id_val(params['target_id']), end_char)
 
